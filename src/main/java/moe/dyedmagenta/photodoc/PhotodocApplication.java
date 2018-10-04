0abcef4d050e9f447e4eaf91dd7f6a1498a40e37
dyedMagenta <frankiewicz@o2.pl>
2018-10-04 20:43:20 +0200

    Empty repo

---
+++ src/main/java/moe/dyedmagenta/photodoc/PhotodocApplication.java
@@ -0,0 +1,12 @@
+package moe.dyedmagenta.photodoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotodocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotodocApplication.class, args);
+	}
+}
---
+++ src/test/java/moe/dyedmagenta/photodoc/PhotodocApplicationTests.java
@@ -0,0 +1,16 @@
+package moe.dyedmagenta.photodoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotodocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
